fd8e98e7c69eeb7842f77f4aa70c4b8e60e1e771
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-06 05:23:11 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -6,12 +6,12 @@ end
 # Define your WIN_COMBINATIONS constant
 board = ["X", "X", "X", " ", " ", " ", " ", " ", " "]
 WIN_COMBINATIONS = [
-  [0,1,2] #top rows
-  [3,4,5] #middle rows
-  [6,7,8] #bottom rows
-  [0,3,6] #left columns
-  [1,4,7] #middle columns
-  [2,5,8] #right columns
-  [2,4,6] #starting from the right
+  [0,1,2], #top rows
+  [3,4,5], #middle rows
+  [6,7,8], #bottom rows
+  [0,3,6], #left columns
+  [1,4,7], #middle columns
+  [2,5,8], #right columns
+  [2,4,6], #starting from the right
   [0,4,8] #starting from the left
   ]
