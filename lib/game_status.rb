c73b2e04700a294337e8dcce7c5914639f829106
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-06 05:44:24 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -28,7 +28,6 @@ def won?(board)
     if position_1 == "X" && position_2 == "X" && position_3 == "X"
       return win_combination # return the win_combination indexes that won.
     elsif position_1 == " " && position_2 == " " && position_3 == " "
-      return win_combination
       false
     end
   end
