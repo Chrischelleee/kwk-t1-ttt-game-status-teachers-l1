e01108f9ce2e09e866224ee398bcdf94634a2b32
Chrischelle Acelajado <chrischelleacelajado@gmail.com>
2018-06-06 05:56:08 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -25,12 +25,13 @@ def won?(board)
   position_3 = board[win_index_3]
 
 if position_1 == " " && position_2 == " " && position_3 == " "
-  return false
-elsif position_1 == "X" && position_2 == "O" && position_3 == "X"
-      return false # return the win_combination indexes that won.
+return false
+  elsif position_1 == "X" && position_2 == "O" && position_3 == "X"
+  return false # return the win_combination indexes that won.
     elsif position_1 == "X" && position_2 == "X" && position_3 == "X"
-          return win_combination
-            else
+    return win_combination
+      elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
               false
     end
   end
